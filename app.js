ad39e33ee977bc8f9f33532bf027c6ad5ded2df7
mkrn52 <pandhermankaran@gmail.com>
2023-08-30 02:58:11 -0600

    change jump link margins

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const p1_div = document.getElementsByClassName('p1')[0];
 const p2_div = document.getElementsByClassName('p2')[0];
 const project_title = document.getElementsByClassName('projectTitle')[0];
 
-var p1images =  document.querySelectorAll('.p1ImgDiv img');
+var p1images =  document.querySelectorAll('.p1 img');
 var p2images =  document.querySelectorAll('.p2ImgDiv img');
 var dotsp1 = document.querySelectorAll('.switchDot1');
 var dotsp2 = document.querySelectorAll('.switchDot2');
